3c8cd34d7624a766487ae63b210d3e1921a4c550
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-19 18:55:52 -0500

    se agrega findByLastName

--- src/main/java/com/example/demo/repositories/IEmployeeJpaRepository.java
+++ src/main/java/com/example/demo/repositories/IEmployeeJpaRepository.java
@@ -4,7 +4,10 @@ import com.example.demo.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long> {
     Employee findByEmployeeId(String employeeId);
+    List<Employee> findByLastName (String lastName);
 }
