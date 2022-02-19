4ee264cbfd3b4b1293190f98d7b014a33387aac7
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-19 18:15:53 -0500

    creacion de modelo project

---
+++ src/main/java/com/example/demo/models/Project.java
@@ -0,0 +1,56 @@
+package com.example.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true, length = 25, nullable = false)
+    private String name;
+
+    public Project() {
+    }
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Project project = (Project) o;
+
+        return id != null ? id.equals(project.id) : project.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
