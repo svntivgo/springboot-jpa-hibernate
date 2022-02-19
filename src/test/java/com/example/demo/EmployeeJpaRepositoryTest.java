311ba32cd9d15c01158e953e2c8f4b03a411f3c0
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-19 18:41:45 -0500

    se agregan proyectos y roles para test

--- src/test/java/com/example/demo/EmployeeJpaRepositoryTest.java
+++ src/test/java/com/example/demo/EmployeeJpaRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
 import com.example.demo.models.Employee;
+import com.example.demo.models.Project;
+import com.example.demo.models.Role;
 import com.example.demo.repositories.IEmployeeJpaRepository;
+import com.example.demo.repositories.IProjectJpaRepository;
+import com.example.demo.repositories.IRoleJpaRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
@@ -18,8 +22,29 @@ public class EmployeeJpaRepositoryTest {
     @Autowired
     private IEmployeeJpaRepository repo;
 
+    @Autowired
+    private IRoleJpaRepository repoRole;
+
+    @Autowired
+    private IProjectJpaRepository repoProject;
+
     @Test
     public void saveEmployee () {
+
+        Role admin = new Role("ROLE_ADMIN");
+        Role dev = new Role("ROLE_DEV");
+
+        admin = repoRole.save(admin);
+        dev = repoRole.save(dev);
+
+        Project projectOne = new Project("Project 1");
+        Project projectTwo = new Project("Project 2");
+        Project projectThree = new Project("Project 3");
+
+        projectOne = repoProject.save(projectOne);
+        projectTwo = repoProject.save(projectTwo);
+        projectThree = repoProject.save(projectThree);
+
         Employee john = new Employee("John", "Smith", "empl123");
         Employee claire = new Employee("Claire", "Simpson", "empl124");
 
