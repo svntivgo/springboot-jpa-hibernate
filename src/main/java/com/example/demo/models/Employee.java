50e97464869d34f22a208176a52b381456bccf52
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-19 14:04:20 -0500

    se crea modelo Employee

---
+++ src/main/java/com/example/demo/models/Employee.java
@@ -0,0 +1,83 @@
+package com.example.demo.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, length = 25)
+    private String firstName;
+
+    @Column(nullable = false, length = 25)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String employeeId;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String firstName, String lastName, String employeeId) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeId = employeeId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        return id != null ? id.equals(employee.id) : employee.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", employeeId='" + employeeId + '\'' +
+                '}';
+    }
+}
