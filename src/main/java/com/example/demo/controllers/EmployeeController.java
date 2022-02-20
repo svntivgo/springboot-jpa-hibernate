5e5b4d0ce0489d8b7edd58cca3e97f9a79951ff9
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-20 18:22:58 -0500

    Se crean controladores para Employee

--- src/main/java/com/example/demo/controllers/EmployeeController.java
+++ src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -16,26 +16,26 @@ public class EmployeeController {
 
     @PostMapping("/create")
     public Employee guardarEmployee (@RequestBody Employee employee) {
-        return employeeService.guardarEmployee(employee);
+        return this.employeeService.guardarEmployee(employee);
     }
 
     @DeleteMapping(path = "/delete/{id}")
     public Boolean eliminarEmployee (@PathVariable("id") Long id) {
-        return employeeService.eliminarEmployee(id);
+        return this.employeeService.eliminarEmployee(id);
     }
 
     @PutMapping(path = "/update")
     public Employee actualizarEmployee(Employee employee) {
-        return employeeService.actualizarEmployee(employee);
+        return this.employeeService.actualizarEmployee(employee);
     }
 
     @GetMapping
     public List<Employee> obtenerTodosEmployee() {
-        return employeeService.obtenerTodosEmployee();
+        return this.employeeService.obtenerTodosEmployee();
     }
 
     @GetMapping(path = "/get/{employeeId}")
     public Employee obtenerEmployeePorEmployeeId(@PathVariable("employeeId") String id) {
-        return employeeService.obtenerEmployeePorEmployeeId(id);
+        return this.employeeService.obtenerEmployeePorEmployeeId(id);
     }
 }
