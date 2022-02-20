15316b3c32837c000976ce7d21b842be09d2bc00
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-20 18:12:45 -0500

    Se crean servicios para Employee

--- src/main/java/com/example/demo/services/EmployeeService.java
+++ src/main/java/com/example/demo/services/EmployeeService.java
@@ -5,6 +5,9 @@ import com.example.demo.repositories.IEmployeeJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeService {
     @Autowired
@@ -12,5 +15,26 @@ public class EmployeeService {
 
     public Employee guardarEmployee (Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    public Boolean eliminarEmployee (Long id) {
+        try {
+            employeeRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Employee actualizarEmployee (Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> obtenerTodosEmployee () {
+        return employeeRepository.findAll();
+    }
+
+    public Employee obtenerEmployeePorEmployeeId (String id) {
+        return employeeRepository.findByEmployeeId(id);
     }
 }
