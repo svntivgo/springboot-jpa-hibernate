5122e43c4944a47e1ce837fed705d83c592761c7
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-20 17:50:19 -0500

    Se crean servicios para Project

--- src/main/java/com/example/demo/services/ProjectService.java
+++ src/main/java/com/example/demo/services/ProjectService.java
@@ -5,6 +5,9 @@ import com.example.demo.repositories.IProjectJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProjectService {
     @Autowired
@@ -12,5 +15,26 @@ public class ProjectService {
 
     public Project guardarProject (Project project) {
         return projectRepository.save(project);
+    }
+
+    public Boolean eliminarProject (Long id) {
+        try {
+            projectRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Project actualizarProject (Project project) {
+        return projectRepository.save(project);
+    }
+
+    public List<Project> obtenerTodosProject () {
+        return projectRepository.findAll();
+    }
+
+    public Optional<Project> obetenerProjectPorId (Long id) {
+        return projectRepository.findById(id);
     }
 }
