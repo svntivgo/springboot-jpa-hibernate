08ab72a776d643d295709d1c96f667efbc73b10e
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-20 21:03:00 -0500

    Comentarios tipo JSDoc en controlador controller

--- src/main/java/com/example/demo/controllers/ProjectController.java
+++ src/main/java/com/example/demo/controllers/ProjectController.java
@@ -15,26 +15,50 @@ public class ProjectController {
     @Autowired
     ProjectService projectService;
 
+    /**
+     * Crea y guarda un projecto en la base de datos
+     * @param project Objeto project con los atributos de un proyecto
+     * @return
+     */
     @PostMapping(path = "/create")
     public Project guardarProject (@RequestBody Project project) {
         return this.projectService.guardarProject(project);
     }
 
+    /**
+     * Elimina un proyecto de la base de datos
+     * @param id del proyecto en la base de datos
+     * @return
+     */
     @DeleteMapping(path = "/delete/{id}")
     public Boolean eliminarProject (@PathVariable("id") Long id) {
         return this.projectService.eliminarProject(id);
     }
 
+    /**
+     * Actualiza y modifica datos de un proyecto
+     * @param project Objeto project incluido su ID en base de datos
+     * @return
+     */
     @PutMapping(path = "/update")
     public Project actualizarProject (@RequestBody Project project) {
         return this.projectService.actualizarProject(project);
     }
 
+    /**
+     * Obtiene todos los proyectos almacenados en la base de datos
+     * @return
+     */
     @GetMapping
     public List<Project> obtenerTodosProject () {
         return this.projectService.obtenerTodosProject();
     }
 
+    /**
+     * Obtiene un proyecto de la base de datos dado su id
+     * @param id del proyecto en la base de datos
+     * @return
+     */
     @GetMapping(path = "/get/{id}")
     public Optional<Project> obtenerProjectPorId (@PathVariable("id") Long id) {
         return this.projectService.obetenerProjectPorId(id);
